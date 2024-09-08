f0de2b7030ba218164537ba8afa4246de93a9df3
Laurent <laurent.roches.simplon@gmail.com>
2024-09-08 15:48:44 +0200

    mise en page inscription faite

--- src/Views/inscription.php
+++ src/Views/inscription.php
@@ -1,57 +1,51 @@
 <?php
 
-include_once __DIR__ . '/Includes/header.php';
+include_once __DIR__ . '/includes/header.php';
 
 $erreur = isset($_SESSION['erreur']) ? $_SESSION['erreur'] : '';
 $_SESSION['erreur'] = '';
 
 ?>
+<div class="form_bg">
+    <div class="form_post_texte">
+        <h2 class="connexion_titre">Inscription</h2>
+        <?php
+        if($erreur !== '') { ?>
+        <p class="erreur_texte"> <?= $erreur ?> </p>
+        <?php } ?>
+        <form class="connexion_form" action="<?=HOME_URL?>inscription" method="POST">
 
-<p>Page pour l'inscription</p>
-<form class="space-y-6" action="<?=HOME_URL?>inscription" method="POST">
+            <input type="hidden" name="dtm_creation" value="<?php echo date('Y-m-d H:i:s'); ?>">
 
-    <input type="hidden" name="dtm_creation" value="<?php echo date('Y-m-d H:i:s'); ?>">
+            <div class="connexion_champs">
+                <label for="str_email" class="">Addresse email :</label>
+                <input id="str_email" name="str_email" type="email" autocomplete="email" required class="">
+            </div>
 
-    <div>
-        <label for="str_email" class="block text-sm font-medium leading-6 text-gray-900">Addresse email :</label>
-        <div class="mt-2">
-          <input id="str_email" name="str_email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-        </div>
+            <div class="connexion_champs">
+                <label for="str_nom" class="">Votre nom :</label>
+                    <input id="str_nom" name="str_nom" type="text" required class="">
+            </div>
+
+            <div class="connexion_champs">
+                <label for="str_prenom" class="">Votre prénom :</label>
+                    <input id="str_prenom" name="str_prenom" type="text" required class="">
+            </div>
+
+            <div class="connexion_champs">
+                <label for="str_pseudo" class="">Votre pseudo :</label>
+                    <input id="str_pseudo" name="str_pseudo" type="text" required class="">
+            </div>
+
+            <div class="connexion_champs">
+                <label for="dtm_naissance" class="">Votre date de naissance :</label>
+                    <input id="dtm_naissance" name="dtm_naissance" type="date" required class="">
+            </div>
+                <button type="submit" class="btn_gd_utilisateur">S'enregistrer</button>
+        </form>
     </div>
+</div>
 
-    <div>
-        <label for="str_nom" class="block text-sm font-medium leading-6 text-gray-900">Votre nom :</label>
-        <div class="mt-2">
-            <input id="str_nom" name="str_nom" type="text" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-        </div>
-    </div>
-
-    <div>
-        <label for="str_prenom" class="block text-sm font-medium leading-6 text-gray-900">Votre prénom :</label>
-        <div class="mt-2">
-            <input id="str_prenom" name="str_prenom" type="text" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-        </div>
-    </div>
-
-    <div>
-        <label for="str_pseudo" class="block text-sm font-medium leading-6 text-gray-900">Votre pseudo :</label>
-        <div class="mt-2">
-            <input id="str_pseudo" name="str_pseudo" type="text" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-        </div>
-    </div>
-
-    <div>
-        <label for="dtm_naissance" class="block text-sm font-medium leading-6 text-gray-900">Votre date de naissance :</label>
-        <div class="mt-2">
-            <input id="dtm_naissance" name="dtm_naissance" type="date" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-        </div>
-    </div>
-
-    <div>
-        <p> <?= $erreur ?> </p>
-        <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">S'enregistrer</button>
-    </div>
-</form>
 <?php
 
-include_once __DIR__ . '/Includes/footer.php';
+include_once __DIR__ . '/includes/footer.php';
